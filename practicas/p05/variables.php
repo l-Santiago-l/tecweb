a87844c53325c19e8e6762651951ad77e3b80918
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-09-03 22:36:50 -0600

    P05 - Archivo base XHTML

---
+++ practicas/p05/variables.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN” “http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title> Variables PHP </title>
+    </head>
+    <body>
+        <?php
+            echo "Esta es una página XHTML";
+        ?>
+        
+    </body>
+</html>
