f81e0e0f9c4b515a87087fdf73bf2ab83c817845
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-09-03 23:00:47 -0600

    P05 - Pregunta 1

--- practicas/p05/variables.php
+++ practicas/p05/variables.php
@@ -7,7 +7,13 @@
     </head>
     <body>
         <?php
-            echo "Esta es una página XHTML";
+            echo '<center><h1> Práctica 5 - Manejo de variables en PHP </h1></center>';
+            echo '<ol>';
+            echo '<li> Determina cuúl de las siguientes variables son validas y explica ¿por qué? </li>';
+            echo '<br> <code> $_myvar | $_7var | $myvar | $var7 | $_element1 </code>';
+            echo '<p> Justificación: Las variables en PHP deben de se indican con el símbolo $, seguido de una letra (mayúscula o minúscula) o guión bajo FORZOSAMENTE, depúes de estos se pueden 
+                    colocar letras, números y caracteres con valores en el rango 127-255 correspondientes en ASCII.</p>';
+            echo '</ol>';
         ?>
         
     </body>
