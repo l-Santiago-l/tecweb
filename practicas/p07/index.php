d40b003da21dc7da4498bae67f8c36c94fca34cd
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-09-12 15:58:34 -0600

    P07 - Ejercicio 1

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -6,6 +6,21 @@
         <title> Métodos GET y POST</title>
     </head>
     <body>
+    <div style="align-items: center; width: 800px; margin-left: 15%;">
+        <h1>Ejercios $GET Y $POST</h1>
         
+        <!-- Ejercicio 1 -->
+        <form method="get">
+            <fieldset>
+                <legend><h2>Ejercicio 1</h2></legend>
+                <p>Escribe un programa para coprobar si un número es múltiplo de 5 y 7</p>
+                <ul>
+                    <li>Número: <input type="number" name="numero"><?php echo numero57()?></li>
+                    
+                </ul>
+                <p><input type="submit" value="Comprobar"></p>
+            </fieldset>
+        </form>
+        </div>
     </body>
 </html>
--- practicas/p07/src/funciones.php
+++ practicas/p07/src/funciones.php
@@ -1,4 +1,14 @@
 <?php
-   
+   function numero57(){
+    if(isset($_GET['numero']) && is_numeric($_GET['numero'])){
+        $num = $_GET['numero'];
+        if ($num%5==0 && $num%7==0){
+            return '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+        }
+        else{
+            return '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+        }
+    }
+}
 
 ?>
