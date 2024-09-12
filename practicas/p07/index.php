9e24abcff079e30c5aa85fac44ee20389b4c647c
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-09-12 15:56:26 -0600

    P07 - Archivos

---
+++ practicas/p07/index.php
@@ -0,0 +1,11 @@
+<?php include 'src/funciones.php'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title> Métodos GET y POST</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
