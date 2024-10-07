e5527efa4616f5f7afd251c5cba71a3002a85572
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-10-07 16:32:00 -0600

    p10 - Correcciones

--- practicas/p10/get_productos_vigentes_v2.php
+++ practicas/p10/get_productos_vigentes_v2.php
@@ -35,6 +35,32 @@
 		<title>Busca tus productos</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <script>
+            function enviarD() {
+                // se obtiene el id de la fila donde está el botón presinado
+                var rowId = event.target.parentNode.parentNode.id;
+
+                // se obtienen los datos de la fila en forma de arreglo
+                var data = document.getElementById(rowId).querySelectorAll(".row-data");
+
+                // Separamos cada dato
+                var nombre = data[0].innerHTML;
+                var marca = data[1].innerHTML;
+                var modelo = data[2].innerHTML;
+                var precio = data[3].innerHTML;
+                var unidades = data[4].innerHTML;
+                var detalles = data[5].innerHTML;
+                var img = (data[6].querySelector("img")).getAttribute('src');
+
+                var urlForm = "formulario_productos_v2.php";
+                var get = "?nombre="+ nombre +"&marca="+ marca +"&modelo="+ modelo +"&precio="+ precio +"&unidades="+ unidades +"&detalles="+ detalles +"&img="+ img;
+                window.open(urlForm+get);
+                /*var urlForm = "formulario.php";
+                var propName = "nombre="+name;
+                var propAge = "edad="+age;
+                window.open(urlForm+"?"+propName+"&"+propAge); */
+            }
+        </script>
 	</head>
 	<body>
         <div style="align-items: center; width: 89%; margin-left: 5%;">
@@ -45,33 +71,36 @@
             <table class="table">
                 <thead class="thead-dark">
                     <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Marca</th>
-                    <th scope="col">Modelo</th>
-                    <th scope="col">Precio</th>
-                    <th scope="col">Unidades</th>
-                    <th scope="col">Detalles</th>
-                    <th scope="col">Imagen</th>
-                    <th scope="col">Editar</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Marca</th>
+                        <th scope="col">Modelo</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Unidades</th>
+                        <th scope="col">Detalles</th>
+                        <th scope="col">Imagen</th>
+                        <th scope="col">Editar</th>
                     </tr>
                 </thead>
                 <tbody>
-                        <?php
-                            foreach($data as $num => $registro) {            // Se recorren tuplas
-                                echo "<tr>";
-                                    echo "<th scope='row'>" . $registro['id'] . "</th>";
-                                    echo "<td>" . $registro['nombre'] . "</td>";
-                                    echo "<td>" . $registro['marca'] . "</td>";
-                                    echo "<td>" . $registro['modelo'] . "</td>";
-                                    echo "<td>" . $registro['precio'] . "</td>";
-                                    echo "<td>" . $registro['unidades'] . "</td>";
-                                    echo "<td>" . $registro['detalles'] . "</td>";
-                                    echo "<td><img src = \"" . $registro['imagen'] . "\" width = 50% height = 30%  ></td>";
-                                    echo "<td><span class='material-symbols-outlined'>edit</span></td>";
-                                echo "</tr>";
-                            }
-                        ?>
+                    <?php
+                        foreach($data as $num => $registro):            // Se recorren tuplas
+                    ?>
+                    <tr id=<? echo '"'.$registro['id'].'"' ?>>
+                        <th scope='row'> <? echo $registro['id'] ?></th>
+                        <td class="row-data"> <? echo $registro['nombre'] ?> </td>
+                        <td class="row-data"> <? echo $registro['marca'] ?> </td>
+                        <td class="row-data"> <? echo $registro['modelo'] ?> </td>
+                        <td class="row-data"> <? echo $registro['precio'] ?> </td>
+                        <td class="row-data"> <? echo $registro['unidades'] ?> </td>
+                        <td class="row-data"> <? echo $registro['detalles'] ?> </td>
+                        <td class="row-data"><img src = <? echo "'".$registro['imagen']."'"?> width = 50% height = 30%  ></td>
+                        <td><input type="image" src="img/editar.svg" onclick="enviarD()"></td>
+                    </tr>
+
+                    <?php
+                        endforeach
+                    ?>
                 </tbody>
             </table>
 
