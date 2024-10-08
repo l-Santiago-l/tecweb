0cd7731fc6a2e750c98eec66bac997f94f9c5c96
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-10-07 21:20:17 -0600

    P10 - Paso de datos correcto

--- practicas/p10/formulario_productos_v2.php
+++ practicas/p10/formulario_productos_v2.php
@@ -46,18 +46,19 @@
                         <fieldset>
                             <legend> Nombre </legend> <!-- Requerido y tener 100 caracteres o menos" -->
                             <label id="lblNombre" class="lblsCa">100</label>
-                            <input type="text" value="<?= !empty($_POST['nombre'])?$_POST['nombre']:$_GET['nombre'] ?>" name="nombre" id="inNom" class="formI" oninput="validarTam(event, this, 'lblNombre');" onkeypress="vacio(this, 'lblNombre', 100);" onblur="validarObligatoriosL(this, 'lblNombreA');"><br/>
+                            <input type="text" value="<?= !empty($_POST['nombre'])?$_POST['nombre']:$_GET['nombre'] ?>" name="nombre" id="inNom" class="formI" oninput="validarTam(event, this, 'lblNombre', 100);" onblur="validarObligatoriosL(this, 'lblNombreA');"><br/>
                             <label id="lblNombreA" class="lblsAdv"><strong> Este campo es obligatorio</strong></label>
                         </fieldset>
                         <br/>
                         <fieldset>
                             <legend> Marca </legend> <!-- Requerido y seleccionarse de una lista de opciones -->
-                            <select name="marca" class="formI" size="1" id="inMarca" value="<?= !empty($_POST['marca'])?$_POST['marca']:$_GET['marca'] ?>">
+                            <select name="marca" class="formI" size="1" id="inMarca">
                                 <option value="M1">Modelo 1</option>
                                 <option value="M2">Modelo 2</option>
                                 <option value="M3">Modelo 3</option>
                                 <option value="M4">Modelo 4</option>
                             </select>
+                            <!-- Considero que esta lista se tiene que llenar dinamicamente, por lo cual realizar todo los cambios para que se muestre la marca enviada por GET/POST aún no aplicarían-->
                             <!--<input type="text" name="marca" class="formI" onblur="validarObligatorios(this, 'lblMarcaA');"> -->
                             <label id="lblMarcaA" class="lblsAdv"><strong> Este campo es obligatorio</strong></label>
                         </fieldset>
@@ -65,7 +66,7 @@
                         <fieldset>
                             <legend> Modelo </legend> <!-- Requerido texto alfanumerico y tener 25 caracteres o menos -->
                             <label id="lblModelo" class="lblsCa">25</label>
-                            <input type="text" value="<?= !empty($_POST['modelo'])?$_POST['modelo']:$_GET['modelo'] ?>" name="modelo" id="inModelo" class="formI" oninput="validarTam(event, this, 'lblModelo');" onkeypress="vacio(this, 'lblModelo', 25);" onblur="validarObligatoriosL(this, 'lblModeloA');">
+                            <input type="text" value="<?= !empty($_POST['modelo'])?$_POST['modelo']:$_GET['modelo'] ?>" name="modelo" id="inModelo" class="formI" oninput="validarTam(event, this, 'lblModelo', 25);" onblur="validarObligatoriosL(this, 'lblModeloA');">
                             <label id="lblModeloA" class="lblsAdv"><strong> Este campo es obligatorio</strong></label>
                         </fieldset>
                         <br/>
@@ -77,7 +78,7 @@
                         <br/>
                         <fieldset>
                             <legend> Unidades </legend>  <!-- Requerido mayor o igual a 0 -->
-                            <input type="number" value="<?= !empty($_POST['unidades'])?$_POST['unidades']:$_GET['unidades'] ?>" name="unidades" id="inUni" class="formI" onblur="validarObligatoriosN(this, 'lblUnidadesA', 0);" onkeypress="return valNumNa(event)">
+                            <input type="number" value="<?= !empty($_POST['unidades'])? (int)$_POST['unidades'] : (int)$_GET['unidades'] ?>" name="unidades" id="inUni" class="formI" onblur="validarObligatoriosN(this, 'lblUnidadesA', 0);" onkeypress="return valNumNa(event)">
                             <label id="lblUnidadesA" class="lblsAdv"><strong> Este campo es obligatorio</strong></label>
                         </fieldset>
                         <br/> 
@@ -89,7 +90,7 @@
                         <fieldset>
                             <legend> Detalles del producto </legend>  <!-- Opcionales debe tener 250 o menos -->
                             <label id="lblDatelles" class="lblsCa">250</label>
-                            <textarea name="detalles" text="<?= !empty($_POST['detalles'])?$_POST['detalles']:$_GET['detalles'] ?>" id="inDet" rows="4" cols="60" class="formI" placeholder="No más de 250 caracteres de longitud" oninput="validarTam(event, this, 'lblDatelles');" onkeypress="vacio(this, 'lblDatelles', 250);" onblur="validarObligatorios(this, 'lblDatellesA');"></textarea>
+                            <textarea name="detalles" id="inDet" rows="4" cols="60" class="formI" placeholder="No más de 250 caracteres de longitud" oninput="validarTam(event, this, 'lblDatelles', 250);" onblur="validarObligatorios(this, 'lblDatellesA');"><?php echo!empty($_POST['detalles']) ? $_POST['detalles'] : $_GET['detalles']; ?></textarea>
                             <label id="lblDatellesA" class="lblsAdv"  ><strong> Este campo es obligatorio</strong></label>
                         </fieldset>
                         
