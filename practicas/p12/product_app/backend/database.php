175c225af922e67a7d6eada17eafba0186ef40b5
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-10-22 01:28:22 -0600

    P12 - Modificaciones para el correcto funcionamiento

--- practicas/p12/product_app/backend/database.php
+++ practicas/p12/product_app/backend/database.php
@@ -2,7 +2,7 @@
     $conexion = @mysqli_connect(
         'localhost',
         'root',
-        '12345678a',
+        '1234',
         'marketzone'
     );
 
--- practicas/p12/product_app/backend/product-add.php
+++ practicas/p12/product_app/backend/product-add.php
@@ -15,7 +15,7 @@
 	    $result = $conexion->query($sql);
         
         if ($result->num_rows == 0) {
-            $conexion->set_charset("utf8");
+            #$conexion->set_charset("utf8");
             $sql = "INSERT INTO productos VALUES (null, '{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}', {$jsonOBJ->precio}, '{$jsonOBJ->detalles}', {$jsonOBJ->unidades}, '{$jsonOBJ->imagen}', 0)";
             if($conexion->query($sql)){
                 $data['status'] =  "success";
