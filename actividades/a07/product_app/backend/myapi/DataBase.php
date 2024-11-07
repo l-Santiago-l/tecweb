14817063b5d64c271e77098cd5a04d54bb6a5637
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-11-07 00:52:31 -0600

    a07 - Métodos a las clases

---
+++ actividades/a07/product_app/backend/myapi/DataBase.php
@@ -0,0 +1,17 @@
+<?php
+class DataBase {
+    protected $conexion;
+
+    public function __construct($user, $pass, $db) {
+        $this->conexion = @mysqli_connect(
+            'localhost',
+            $user,
+            $pass,
+            $db
+        );
+        # Esto es para validar que si se realice la conexión
+        if(!$this->conexion)
+            $this->conexion = Null;
+    }
+}
+?>
