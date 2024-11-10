1b3863da8b357ba96456aa13b2db653096ece3e9
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-11-10 16:45:01 -0600

    P13 - Clase DataBase

--- practicas/p13/product_app/backend/DataBase/DataBase.php
+++ practicas/p13/product_app/backend/DataBase/DataBase.php
@@ -1,3 +1,23 @@
 <?php
-   
+    abstract class DataBase {
+        protected $conexion;
+        protected $data;
+
+        public function __construct($user, $pass, $db) {
+            $this->conexion = @mysqli_connect(
+                'localhost',
+                $user,
+                $pass,
+                $db
+            );
+            $this->data = array();
+            # Esto es para validar que si se realice la conexión
+            if(!$this->conexion)
+                $this->conexion = Null;
+        }
+
+        public function getData(){
+            return $this->data;
+        }
+    }
 ?>
