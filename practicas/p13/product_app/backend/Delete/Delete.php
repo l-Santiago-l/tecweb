b737a4b554592e8e6bd03b009932b2313bb896b0
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-11-10 17:18:34 -0600

    P13 - Clase Delete

--- practicas/p13/product_app/backend/Delete/Delete.php
+++ practicas/p13/product_app/backend/Delete/Delete.php
@@ -7,6 +7,22 @@
         public function __construct($db, $user = 'root', $pass='1234') {
             parent::__construct($user, $pass, $db);
         }
+
+        public function delete($id) {
+            $this->data = array(
+                'status'  => 'error',
+                'message' => 'La consulta falló'
+            );
+            // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+            $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
+            if ( $this->conexion->query($sql) ) {
+                $this->data['status'] =  "success";
+                $this->data['message'] =  "Producto eliminado";
+            } else {
+                $this->data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($this->conexion);
+            }
+            $this->conexion->close();
+        }
     }
 
 
