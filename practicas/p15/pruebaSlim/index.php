99c4b181d1413563b7ca6f822509f90c29922ccc
l-Santiago-l <diego.leonh@alumno.buap.mx>
2024-11-30 19:38:06 -0600

    P15 - Post y JSON

--- practicas/p15/pruebaSlim/index.php
+++ practicas/p15/pruebaSlim/index.php
@@ -10,5 +10,22 @@
         $response->write("Hola, " . $args["nombre"]);
         return $response;
     });
+
+    $app ->post('/pruebapost', function($request, $response, $args){
+        $reqPost = $request->getParsedBody();
+        $val1=$reqPost["val1"];
+        $val2=$reqPost["val2"];
+        $response->write("Valores: ". $val1 . " " . $val2);
+        return $response;
+    });
+
+    $app->get("/testjson", function($request, $response, $args){
+        $data[0]["nombre"]="Diego";
+        $data[0]["apellido"]="León";    
+        $data[1]["nombre"]="Santiago";
+        $data[1]["apellido"]="Hernández";
+        $response->write(json_encode($data, JSON_PRETTY_PRINT));
+    });
+
     $app->run();
 ?>
